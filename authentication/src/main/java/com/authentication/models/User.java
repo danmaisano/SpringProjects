38750601d9e7c7dc8c7c7e0e356a5a09fda4868e
Dan Maisano <danmaisano@gmail.com>
2023-09-03 11:54:06 -0400

    fixed

--- authentication/src/main/java/com/authentication/models/User.java
+++ authentication/src/main/java/com/authentication/models/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -31,6 +31,7 @@ public class User {
     @Size(min=8, max=128, message="Password must be between 8 and 128 characters")
     private String password;
     
+    @Transient
     @NotEmpty(message="Confirm Password is required!")
     @Size(min=8, max=128, message="Confirm Password must be between 8 and 128 characters")
     private String confirm;
